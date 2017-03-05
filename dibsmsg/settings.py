ec6fad0af3359ff9a15050e02ff2dcc505206bf7
aliciant <alicia@ondibs.com>
2017-03-05 16:43:27 -0500

    Starting over with different files

--- dibsmsg/settings.py
+++ dibsmsg/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%7m)eg-5gg^dwtj923cjvkh_xw@))g5n41^yugfsm+zy#s!jno'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
